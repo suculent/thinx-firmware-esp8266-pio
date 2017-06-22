c978dc1f4a8165eebdd17c9c2b35a4928e99f362
Matěj Sychra <suculent@me.com>
2017-06-22 22:48:53 +0200

    base project with all dependencies included (pinning, may be removed later but should keep pinned to version for security purposes anyway)

--- prerelease.sh
+++ prerelease.sh
@@ -7,14 +7,14 @@ echo
 # Use this before your own internal releases. Build server does it automatically.
 
 THINX_FILE=$(find . | grep "/Thinx.h")
-THINX_OWNER="test" # todo: override with parameter
+THINX_OWNER="eaabae0d5165c5db4c46c3cb6f062938802f58d9b88a1b46ed69421809f0bf7f" # todo: override with parameter
 THINX_ALIAS="vanilla" # todo: override with parameter
 THINX_CLOUD_URL="rtm.thinx.cloud" # IP causes crashes
 THINX_MQTT_URL="${THINX_CLOUD_URL}" # mqtt://?
 
 REPO_NAME='thinx-firmware-esp8266'
 VERSION=$(git rev-list HEAD --count)
-REPO_VERSION="1.3.${VERSION}"
+REPO_VERSION="1.6.${VERSION}"
 BUILD_DATE=`date +%Y-%m-%d`
 
 echo "//" > $THINX_FILE
@@ -31,7 +31,7 @@ echo "static const String thinx_firmware_version_short = \"${REPO_VERSION}\";" >
 echo "String thinx_owner = \"${THINX_OWNER}\";" >> $THINX_FILE
 echo "String thinx_alias = \"${THINX_ALIAS}\";" >> $THINX_FILE
 echo "String thinx_api_key = \"VANILLA_API_KEY\";" >> $THINX_FILE
-echo "String thinx_udid = \"unique-device-identifier\";" >> $THINX_FILE
+echo "String thinx_udid = \"\";" >> $THINX_FILE
 
 echo "" >> $THINX_FILE
 
