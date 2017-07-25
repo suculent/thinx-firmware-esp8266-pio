4200aac16f17e392053a5fd9c4dc0962c5e1f2c6
Matěj Sychra <suculent@me.com>
2017-07-25 15:00:56 +0200

    updated version tag, commit will be correct only on machine-build

--- thinx-firmware-esp8266-arduinoc-lib/thinx-lib-esp8266-arduinoc/src/thinx.h
+++ thinx-firmware-esp8266-arduinoc-lib/thinx-lib-esp8266-arduinoc/src/thinx.h
@@ -2,15 +2,15 @@
 // This is an auto-generated file, it will be overwritten by THiNX on cloud build.
 //
 
-#define THINX_ALIAS "eavboard-wemos-mini"
+#define THINX_ALIAS "robotdyn-mega+wifi"
 
 #define THINX_OWNER "cedc16bb6bb06daaa3ff6d30666d91aacd6e3efbf9abbc151b4dcade59af7c12"
 #define THINX_API_KEY "88eb20839c1d8bf43819818b75a25cef3244c28e77817386b7b73b043193cef4"
 
-#define THINX_COMMIT_ID "269c6fa21cf7e02d7db098b1fc20d14b9c8ce600"
-#define THINX_FIRMWARE_VERSION_SHORT "1.6.92"
-#define THINX_FIRMWARE_VERSION "thinx-firmware-esp8266-1.6.92:2017-06-25"
-#define THINX_UDID "41528570-6965-11e7-9b6d-6924bbc83a7c" // should not be here
+#define THINX_COMMIT_ID "24e715f7fb5dc3ebe228165dae45fa933fade1bd"
+#define THINX_FIRMWARE_VERSION_SHORT "1.7.24"
+#define THINX_FIRMWARE_VERSION "thinx-firmware-esp8266-1.7.24:2017-07-25"
+#define THINX_UDID ""
 
 #define THINX_CLOUD_URL "thinx.cloud"
 #define THINX_MQTT_URL "rtm.thinx.cloud"
@@ -20,11 +20,11 @@
 #define THINX_PROXY "thinx.local"
 #define THINX_PLATFORM "platformio"
 
-#define THINX_APP_VERSION "1.0.0"
+#define THINX_APP_VERSION "0.7.0"
 
 // end of machine-generated code
 
 // BEGINNING OF USER DATA
 
-#define wifi_ssid "VINARNA"
-#define wifi_password "najednicku"
+#define wifi_ssid "THiNX-IoT"
+#define wifi_password "<enter-your-ssid-password>"
