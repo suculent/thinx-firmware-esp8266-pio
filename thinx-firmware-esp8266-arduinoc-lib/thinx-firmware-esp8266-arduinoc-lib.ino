e378351b41ba7e3ecb79f6749f0403557a4181dd
Matěj Sychra <suculent@me.com>
2017-08-29 21:24:09 +0200

    crashes on EAVManager::AutoConnect; but so far so good...

--- thinx-firmware-esp8266-arduinoc-lib/thinx-firmware-esp8266-arduinoc-lib.ino
+++ thinx-firmware-esp8266-arduinoc-lib/thinx-firmware-esp8266-arduinoc-lib.ino
@@ -1,93 +1,39 @@
-#include <dummy.h>
-
 /* OTA enabled firmware for Wemos D1 (ESP 8266, Arduino) */
 
+#include <dummy.h>
 #include "Arduino.h"
-#include "FS.h"
 
 #define __DEBUG__ // wait for serial port on boot
 
 // 1. Include the THiNXLib
 #include <THiNXLib.h>
 
-
 // 2. Include your API Key from a file you don't store in repository (use .gitignore)
 #include "Settings.h"
 
-String realSize = String(ESP.getFlashChipRealSize());
-String ideSize = String(ESP.getFlashChipSize());
-bool flashCorrectlyConfigured = realSize.equals(ideSize);
-bool fileSystemReady = false;
+// OR Enter your API Key if not using in Settings.h
+// String apikey = "71679ca646c63d234e957e37e4f4069bf4eed14afca4569a0c74abf503076732";
 
 // 3. Declare
 THiNX thx;
 
 void setup() {
-
   Serial.begin(115200);
-  Serial.setDebugOutput(true);
-
 #ifdef __DEBUG__
-  while (!Serial) {
-      //
-  };
+  while (!Serial);
   delay(500);
-
-
-   Serial.println("* TH: Booting in 5 seconds..."); Serial.flush();
-
-   delay(5000);
-
-   Serial.print("* TH: SPIFFS real size = ");
-   Serial.println(realSize); Serial.flush();
-
-   Serial.print("* TH: SPIFFS IDE size = ");
-   Serial.println(ideSize); Serial.flush();
-
-   Serial.println("* TH: Checking SPIFFS:"); Serial.flush();
-
-   if(flashCorrectlyConfigured) {
-     //Serial.println("* TH: Formatting SPIFFS..."); Serial.flush();
-
-     fileSystemReady = SPIFFS.begin();
-
-     if (!fileSystemReady) {
-       fileSystemReady = SPIFFS.format();;
-       fileSystemReady = SPIFFS.begin();
-     }
-     Serial.println("* TH: Formatting completed. Calling SPIFFS.begin in 1sec..."); Serial.flush();
-     delay(1000);
-     //fileSystemReady = SPIFFS.begin();
-     Serial.println("."); Serial.flush();
-
-     if (!fileSystemReady) {
-       Serial.println("*TH: Formatting SPIFFS..."); ; Serial.flush();
-       SPIFFS.format();
-       Serial.println("*TH: Re-mounting SPIFFS..."); ; Serial.flush();
-       fileSystemReady = SPIFFS.begin();
-     } else {
-      Serial.println("*TH: SPIFFS mounted.");
-     }
-   }  else {
-     Serial.println("flash incorrectly configured, SPIFFS cannot start, IDE size: " + ideSize + ", real size: " + realSize);
-     return;
-   }
+  Serial.setDebugOutput(true);
 #endif
 
   // 4. Initialize
-  Serial.println("*TH: Initializing in 5 seconds...");
-
-  delay(5000);
-
+  Serial.println("*TH: Initializing...");
   thx = THiNX(apikey);
 }
 
 void loop()
 {
   delay(10000);
-  Serial.printf("Free size: %u\n", ESP.getFreeSketchSpace());
-
-
   // 5. Waits for WiFI, register, check MQTT, reconnect, update...
-  //thx.loop();
+  thx.loop();
+  Serial.printf("Free size: %u\n", ESP.getFreeSketchSpace());
 }
