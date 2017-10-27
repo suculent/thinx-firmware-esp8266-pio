a0c285e219e0feadee969fe65c86684dc8e6867d
Matěj Sychra <suculent@me.com>
2017-10-27 13:58:03 +0200

    significant stability fixes thanx to code-review by @Develo

--- thinx-firmware-esp8266-arduinoc-lib/thinx-firmware-esp8266-arduinoc-lib.ino
+++ thinx-firmware-esp8266-arduinoc-lib/thinx-firmware-esp8266-arduinoc-lib.ino
@@ -18,32 +18,26 @@ void setup() {
   delay(3000);
 #endif
 
-/*
 #ifndef __USE_WIFI_MANAGER__
   // Force-override WiFi before attempting to connect in case we don't use EAVManager
-  // or WiFiManager with configuration from Settings.h
-  ETS_UART_INTR_DISABLE();
-  WiFi.disconnect();
-  ETS_UART_INTR_ENABLE();
+  // or WiFiManager with configuration from thinx.h
+  //ETS_UART_INTR_DISABLE();
+  //WiFi.disconnect();
+  //ETS_UART_INTR_ENABLE();
   WiFi.mode(WIFI_STA);
-  Serial.println("*INO: WiFi mode STA");
-  //WiFi.begin();
-
+  Serial.println(F("*INO: WiFi mode STA"));
+  WiFi.begin();
   if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("*INO: Connected WiFi using saved credentials.");
+    Serial.println(F("*INO: Connected WiFi using saved credentials."));
   } else {
-    Serial.println("*INO: Connect WiFi with build-configuration...");
-    WiFi.begin("THiNX-IoT+", "<enter-your-ssid-password>");
-    delay(2000); // wait for DHCP, otherwise falls to AP mode
+    Serial.println(F("*INO: Connect WiFi with build-configuration..."));
+    WiFi.begin("THiNX-IoT+", "<enter-your-ssid-password>"); // enter your WiFi credentials here
+    delay(2000); // wait for DHCP, otherwise falls back to AP mode
   }
 #endif
-*/
 
-  ETS_GPIO_INTR_DISABLE();
   thx = THiNX("71679ca646c63d234e957e37e4f4069bf4eed14afca4569a0c74abf503076732"); // API Key
   first_loop = false;
-  ETS_GPIO_INTR_ENABLE();
-
 }
 
 // ICACHE_RAM_ATTR ?
@@ -56,7 +50,6 @@ unsigned long frame_counter = 0;
 void loop()
 {
   if (first_loop) {
-    // thx = THiNX("71679ca646c63d234e957e37e4f4069bf4eed14afca4569a0c74abf503076732"); // API Key
     first_loop = false;
     Serial.println(" ");
     Serial.println("» THiNX successfully initialized.");
@@ -65,14 +58,15 @@ void loop()
     thx.loop();
 
     // Prints millis every 100.000th frame, resets counter
-    /*
+
     frame_counter++;
     if (frame_counter % 10000 == 0) {
       Serial.println(millis());
       frame_counter = 0;
     } else {
       delay(1);
-    } crash? */
+    }
   }
   delay(1);
+
 }
