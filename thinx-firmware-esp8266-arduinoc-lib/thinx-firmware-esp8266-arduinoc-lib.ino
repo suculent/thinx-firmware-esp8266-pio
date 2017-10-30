5e6bb79ae30caf75a296c02dcd6ca10e6a6b6466
Matěj Sychra <suculent@me.com>
2017-10-31 00:11:35 +0100

    updated lib to 2.0.99, simplified example with both API Key/Owner ID

--- thinx-firmware-esp8266-arduinoc-lib/thinx-firmware-esp8266-arduinoc-lib.ino
+++ thinx-firmware-esp8266-arduinoc-lib/thinx-firmware-esp8266-arduinoc-lib.ino
@@ -9,8 +9,6 @@
 // 3. Declare
 THiNX thx;
 
-bool first_loop = true;
-
 /* ICACHE_RAM_ATTR */ void finalizeCallback () {
   Serial.println("*INO: Finalize callback called.");
 }
@@ -23,12 +21,9 @@ void setup() {
   delay(3000);
 #endif
 
-  thx = THiNX("71679ca646c63d234e957e37e4f4069bf4eed14afca4569a0c74abf503076732"); // Enter API Key
+  thx = THiNX("71679ca646c63d234e957e37e4f4069bf4eed14afca4569a0c74abf503076732", "cedc16bb6bb06daaa3ff6d30666d91aacd6e3efbf9abbc151b4dcade59af7c12"); // Enter API Key and Owner ID
   thx.setFinalizeCallback(finalizeCallback); // called after library gets connected and registered
-  first_loop = false;
 }
-
-unsigned long frame_counter = 0;
 
 void loop()
 {
