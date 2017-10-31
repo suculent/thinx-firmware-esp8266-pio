39172e44024e67d9204ff3ac416081a0077a6ad3
Matěj Sychra <suculent@me.com>
2017-10-31 11:27:52 +0100

    simplified example, added WiFIManager to libs (should be a submodule)

--- thinx-firmware-esp8266-arduinoc-lib/thinx-firmware-esp8266-arduinoc-lib.ino
+++ thinx-firmware-esp8266-arduinoc-lib/thinx-firmware-esp8266-arduinoc-lib.ino
@@ -1,15 +1,10 @@
 #include "Arduino.h"
-
-// 1. Include the THiNXLib
 #include <THiNXLib.h>
 
-// 2. Find 'thinx.h' in THiNXLib and enter your THINX_OWNER identifier
-// from RTM Console > User Profile > Overview
-
-// 3. Declare
 THiNX thx;
 
-/* ICACHE_RAM_ATTR */ void finalizeCallback () {
+/* Called after library gets connected and registered */
+void finalizeCallback () {
   Serial.println("*INO: Finalize callback called.");
 }
 
@@ -21,10 +16,12 @@ void setup() {
   delay(3000);
 #endif
 
-  thx = THiNX("71679ca646c63d234e957e37e4f4069bf4eed14afca4569a0c74abf503076732", "cedc16bb6bb06daaa3ff6d30666d91aacd6e3efbf9abbc151b4dcade59af7c12"); // Enter API Key and Owner ID
-  thx.setFinalizeCallback(finalizeCallback); // called after library gets connected and registered
+   // Enter API Key and Owner ID
+  thx = THiNX("71679ca646c63d234e957e37e4f4069bf4eed14afca4569a0c74abf503076732", "cedc16bb6bb06daaa3ff6d30666d91aacd6e3efbf9abbc151b4dcade59af7c12");
+  thx.setFinalizeCallback(finalizeCallback);
 }
 
+/* Loop must call the thx.loop() in order to pickup MQTT messages and advance the state machine. */
 void loop()
 {
   thx.loop();
