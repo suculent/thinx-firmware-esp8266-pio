f72c1fd6d647d0c1e01311964de8db2042b1093a
Matěj Sychra <suculent@me.com>
2017-11-05 22:04:10 +0100

    added sample with setting status

--- thinx-esp8266-example/thinx-esp8266-example.ino
+++ thinx-esp8266-example/thinx-esp8266-example.ino
@@ -6,7 +6,7 @@ THiNX thx;
 /* Called after library gets connected and registered */
 void finalizeCallback () {
   Serial.println("*INO: Finalize callback called.");
-  ESP.deepSleep(3e9);
+  //ESP.deepSleep(3e9);
 }
 
 /* Example of using Environment variables */
@@ -54,6 +54,7 @@ void setup() {
 
    // Enter API Key and Owner ID
   thx = THiNX("71679ca646c63d234e957e37e4f4069bf4eed14afca4569a0c74abf503076732", "cedc16bb6bb06daaa3ff6d30666d91aacd6e3efbf9abbc151b4dcade59af7c12");
+  thx.setStatus("Hello internet!");
   thx.setFinalizeCallback(finalizeCallback);
   thx.setPushConfigCallback(pushConfigCallback);
 }
